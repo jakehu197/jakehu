8a76908436daee3d856ce364d3ba51116f3b2060
jakehu197 <62554058+jakehu197@users.noreply.github.com>
2024-05-31 21:19:08 +0800

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,28 @@
+$(function() {
+
+	var offset = 300,
+			offset_opacity = 200,
+			scroll_top_duration = 300,
+			$back_to_top = $('.buttontop-top');
+
+	$(window).scroll(function(){
+
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('buttontop-is-visible') : $back_to_top.removeClass('buttontop-is-visible buttontop-fade-out');
+
+		if( $(this).scrollTop() > offset_opacity ) {
+			$back_to_top.addClass('buttontop-fade-out');
+		}
+
+	});
+
+	$back_to_top.on('click', function(event){
+
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+			 }, scroll_top_duration
+		);
+
+	});
+
+});
